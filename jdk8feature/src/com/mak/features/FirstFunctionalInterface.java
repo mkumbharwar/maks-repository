096304834ea541a5fa83536b3bed909120eeb928
mkumbharwar <mkumbharwar@gmail.com>
2018-06-24 14:57:33 +0530

    Change-Id: I3ac6fd5375d65b6a77ee586013ecc42008abb219 Signed-off-by: mkumbharwar <mkumbharwar@gmail.com>

---
+++ jdk8feature/src/com/mak/features/FirstFunctionalInterface.java
@@ -0,0 +1,8 @@
+package com.mak.features;
+
+@FunctionalInterface
+public interface FirstFunctionalInterface {
+
+	public String workTodo(String name);
+
+}
---
+++ jdk8feature/src/com/mak/features/TestLambdaExpression.java
@@ -0,0 +1,33 @@
+package com.mak.features;
+
+/**
+ * @author Mak
+ *
+ *         Lambda expression
+ *         <h1>either (parameters) -> expression or (parameters) -> {
+ *         statements; } or () -> expression</h1>
+ * 
+ *         In programming, a Lambda expression (or function) is just an
+ *         anonymous function, i.e., a function with no name and without being
+ *         bounded to an identifier. They are written exactly in the place where
+ *         it’s needed, typically as a parameter to some other function.
+ */
+public class TestLambdaExpression {
+
+	public static void main(String[] args) {
+
+		FirstFunctionalInterface s = (name) -> {
+			return name + " Testing labda expression!!";
+		};
+		System.out.println(s.workTodo("Mahesh"));
+		
+		FirstFunctionalInterface sAnonymous =new FirstFunctionalInterface() {
+			
+			@Override
+			public String workTodo(String name) {
+				return name+" Anonymous testing!!!";
+			}
+		};
+		System.out.println(sAnonymous.workTodo("Bamman"));
+	}
+}
